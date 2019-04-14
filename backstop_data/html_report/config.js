c75b1de8fc9be2271ba65aeac99a78af9c0bf20c
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-14 11:01:07 +0300

    initial commit

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,55 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Test_0_data-qa-card_0_tablet.png",
+        "test": "../bitmaps_test/20190414-105828/backstop_default_Test_0_data-qa-card_0_tablet.png",
+        "selector": "[data-qa-card]",
+        "fileName": "backstop_default_Test_0_data-qa-card_0_tablet.png",
+        "label": "Test",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:3000",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Test_Hover_0_data-qa-card_0_tablet.png",
+        "test": "../bitmaps_test/20190414-105828/backstop_default_Test_Hover_0_data-qa-card_0_tablet.png",
+        "selector": "[data-qa-card]",
+        "fileName": "backstop_default_Test_Hover_0_data-qa-card_0_tablet.png",
+        "label": "Test Hover",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:3000",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.03",
+          "analysisTime": 10
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
