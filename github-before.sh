59ebf9ff6ddc8de22ae77918e91ab8dc6bccf4b7
Katia Ilchenko <k17ilch@gmail.com>
2019-04-14 23:06:37 +0300

    config

--- github-before.sh
+++ github-before.sh
@@ -2,4 +2,5 @@ git status
 git config user.email "dev@mate.academy"
 git config user.name "Mate academy DEV"
 git config credential.helper "store --file=.git/credentials"
+git config --global credential.helper wincred
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
