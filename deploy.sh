5390be67edbe9a5b339172bb357c51e96c141822
Katia Ilchenko <k17ilch@gmail.com>
2019-04-14 22:55:11 +0300

    config

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+
 cp -R backstop_data/ src/report
 git add src/
 git commit -m "deploy static content"
-git subtree push --prefix src origin gh-pages
+git subtree push --prefix src "https://github.com/${TRAVIS_PULL_REQUEST_SLUG}.git" master:gh-pages
--- github-before.sh
+++ github-before.sh
@@ -3,8 +3,3 @@ git config user.email "dev@mate.academy"
 git config user.name "Mate academy DEV"
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-
-git remote -v
-echo ${TRAVIS_PULL_REQUEST}
-echo ${TRAVIS_PULL_REQUEST_SLUG}
-echo ${TRAVIS_BRANCH}
