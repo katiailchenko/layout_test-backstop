374520b3de437e7b2afc82453974014e8c07544b
Katia Ilchenko <k17ilch@gmail.com>
2019-04-14 22:59:17 +0300

    config

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@
 cp -R backstop_data/ src/report
 git add src/
 git commit -m "deploy static content"
-git subtree push --prefix src "https://github.com/${TRAVIS_PULL_REQUEST_SLUG}.git" master:gh-pages
+git subtree push --prefix src "https://github.com/${TRAVIS_PULL_REQUEST_SLUG}.git" gh-pages
