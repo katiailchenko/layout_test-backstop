faa313228f47a06a1079f27b03c9b0d05e744068
Katia Ilchenko <k17ilch@gmail.com>
2019-04-14 22:31:51 +0300

    config

--- github-before.sh
+++ github-before.sh
@@ -3,3 +3,7 @@ git config user.email "dev@mate.academy"
 git config user.name "Mate academy DEV"
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+
+git remote -v
+echo TRAVIS_BRANCH
+echo ${TRAVIS_BRANCH}
